321fd61fbdafa4be26f20a93e139b206bd896066
Samuel <samuel.tocci@gmail.com>
2022-03-22 15:50:44 +0100

    overview layout init + renaming

--- app/src/main/java/com/ee5/mobile/LoginActivity.java
+++ app/src/main/java/com/ee5/mobile/LoginActivity.java
@@ -6,12 +6,12 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         ConstraintLayout constraintLayout = findViewById(R.id.login_layout);
         AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
---
+++ app/src/main/java/com/ee5/mobile/OverviewActivity.java
@@ -0,0 +1,21 @@
+package com.ee5.mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.overview_layout);
+
+        ProgressBar progressBar = findViewById(R.id.);
+        progressBar.setProgress(50);
+    }
+}
