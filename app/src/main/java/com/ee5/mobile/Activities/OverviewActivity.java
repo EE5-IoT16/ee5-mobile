df697233281e0cb7e6cf8458c9100db60b0fc95d
Samuel <samuel.tocci@gmail.com>
2022-03-24 14:17:58 +0100

    Quote coloring and positioning programatically added

--- app/src/main/java/com/ee5/mobile/Activities/OverviewActivity.java
+++ app/src/main/java/com/ee5/mobile/Activities/OverviewActivity.java
@@ -5,11 +5,18 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.ee5.mobile.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class OverviewActivity extends AppCompatActivity {
 
@@ -21,6 +28,7 @@ public class OverviewActivity extends AppCompatActivity {
         ConstraintLayout constraintLayout = findViewById(R.id.overview_layout);
 
         Button viewProfile_btn = findViewById(R.id.viewProfile_Btn);
+        TextView stepsLeft_num = findViewById(R.id.stepsLeft_num);
         TextView quote = findViewById(R.id.quote_tv);
 
         viewProfile_btn.setOnClickListener(v -> {
@@ -28,5 +36,31 @@ public class OverviewActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+
+
+        //TODO: get number from User object
+        stepsLeft_num.setText("100");
+
+        String quoteString = "Look in the mirror, that’s the only competition.";
+
+        //quoteString = getColoredText();
+
+        //quote = get from http request to some api
+        quote.setText(getColoredText(quoteString, "#4FA4FF"));
+
+    }
+
+    public Spanned getColoredText(String text, String color) {
+        String[] words = text.split(" ");
+        int wordCount = words.length;
+
+        int backSpace = 4;
+        words[backSpace] = words[backSpace];
+
+        String[] beforeColor = Arrays.copyOfRange(words,0,backSpace);
+        String[] afterColor = Arrays.copyOfRange(words, backSpace +1, wordCount);
+
+        String output = String.join(" ",beforeColor)+ " " + "<font color=" + color + ">" + words[backSpace] + "</font>"+ "<br> " +String.join(" ",afterColor);
+        return Html.fromHtml(output);
     }
 }
