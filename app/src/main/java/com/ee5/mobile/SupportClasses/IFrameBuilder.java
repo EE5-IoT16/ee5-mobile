649535380d2f6930072d9e1cf5cc7c9333400ac8
Samuel Tocci <samuel.tocci@student.kuleuven.be>
2022-05-03 13:31:40 +0200

    ssid dataframe with byte input fixed

--- app/src/main/java/com/ee5/mobile/SupportClasses/IFrameBuilder.java
+++ app/src/main/java/com/ee5/mobile/SupportClasses/IFrameBuilder.java
@@ -11,8 +11,9 @@ public interface IFrameBuilder {
 
     static byte[] getSsidDataFrame(byte[] ssid, int sequence) {
         byte[] dataFrame = new byte[6 + ssid.length];
-        dataFrame[0] = (byte) 00_000100; //Type & Subtype
-        dataFrame[1] = (byte) 000_00000; //Frame control with 3MSB reserved
+        dataFrame[0] = (byte) 0x01; //Type & Subtype
+        dataFrame[0] = (byte) ((0x02<<2) | dataFrame[0]);
+        dataFrame[1] = (byte) 0x00; //Frame control with 3MSB reserved
         dataFrame[2] = (byte) sequence;
         dataFrame[3] = (byte) ssid.length;
         int i =0;
